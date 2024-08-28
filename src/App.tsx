df382f2cdcf105ec840abdc135fcfb9652ca36c4
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-08-28 18:25:34 +0500

    learned about how to use quill text editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 function App() {
+  const [value, setValue] = useState("");
+
   return (
-    <main>
-      {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </main>
+    <div className="w-[900px] mx-auto mt-[40px]">
+      <div>
+        <ReactQuill
+          theme="snow"
+          value={value}
+          onChange={setValue}
+          className="h-[400px]"
+        />
+      </div>
+
+      <div className="mt-[40px]">
+        <h1>Output</h1>
+        <div>{value}</div>
+      </div>
+    </div>
   );
 }
 
